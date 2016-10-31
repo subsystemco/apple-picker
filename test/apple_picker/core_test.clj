773a246995513552787589100ae97f6d5c641741
Greg Leppert <greg@leppert.me>
2016-10-31 16:19:51 -0400

    Add tests

--- test/apple_picker/core_test.clj
+++ test/apple_picker/core_test.clj
@@ -1,7 +1,27 @@
 (ns apple-picker.core-test
   (:require [clojure.test :refer :all]
-            [apple-picker.core :refer :all]))
+            [apple-picker.core :refer :all]
+            [apple-picker.http :as http]
+            [apple-picker.fixture :as fixture]
+            [clojure.core.async :refer [<!!]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest receipt-verification-works
+  (testing "a valid receipt is correctly returned"
+    (let [receipt (:sandbox-expired fixture/receipts)
+          response (<!! (verify-receipt (:receipt-data receipt) (:password receipt)))]
+      (is (= (get-in response [:body :receipt :receipt_type]) "ProductionSandbox"))))
+
+  (testing "a receipt is validated with sandbox if code returned"
+    (let [receipt (:sandbox-expired fixture/receipts)
+          response (<!! (verify-receipt (:receipt-data receipt) (:password receipt)))]
+      (is (= (get-in response [:body :receipt :receipt_type]) "ProductionSandbox"))))
+
+  (testing "an invalid receipt is correctly returned"))
+
+(defn scratch []
+  (let [receipt (:sandbox-expired fixture/receipts)]
+    (<!! (verify-receipt (:receipt-data receipt) (:password receipt))))
+
+(let [receipt (:sandbox-expired fixture/receipts)]
+  (<!! (http/post "https://buy.itunes.apple.com/verifyReceipt" receipt)))
+  )
