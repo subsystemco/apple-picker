4dad3bb5e8e87ff2af5c123f900f6ed83fd0146f
Greg Leppert <greg@leppert.me>
2016-11-14 19:56:58 -0500

    Use two-headed-boy

--- src/apple_picker/utils.cljs
+++
@@ -1,9 +0,0 @@
-(ns apple-picker.utils
-  (:require [cljs.nodejs :as nodejs]
-            [cljs.reader :refer [read-string]]))
-
-(defn read-data
-  [path]
-  (-> (nodejs/require "fs")
-      (.readFileSync path "UTF-8")
-      read-string))
